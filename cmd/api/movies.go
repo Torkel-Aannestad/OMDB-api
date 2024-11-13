15a529b0d0cee8cd344ddc3ba7c9c5321c939f44
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-11-13 18:48:22 +0100

    writeJson method

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -14,7 +14,13 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "showMovieHandler")
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
 }
 
 func (app *application) editMovieHandler(w http.ResponseWriter, r *http.Request) {
