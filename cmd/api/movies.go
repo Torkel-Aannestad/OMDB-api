5a51d50c5982273a3251c0f57ddd334c4d816e75
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-11-13 19:01:29 +0100

    Movie struct

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/Torkel-Aannestad/MovieMaze/internal/data"
 )
 
 func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,7 +23,20 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
 
 func (app *application) editMovieHandler(w http.ResponseWriter, r *http.Request) {
