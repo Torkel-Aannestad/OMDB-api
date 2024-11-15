6f00627c36a2232ab273bf301c331b1c42510ad9
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-11-15 10:04:24 +0100

    deleteMovieHandler done

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -136,5 +136,27 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	err = app.model.DeleteMovie(r.Context(), id)
+	if err != nil {
+		switch {
+		case errors.Is(err, ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": "movie was successfully deleted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+
+	}
 
 }
