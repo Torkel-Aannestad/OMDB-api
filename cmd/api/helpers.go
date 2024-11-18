3b0023a41a1661aa48eab5619aa56630b8214b05
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-11-18 13:26:26 +0100

    background job helper function

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -147,3 +147,18 @@ func (app *application) readCSV(qs url.Values, key string, defaultValue []string
 
 	return strings.Split(csv, ",")
 }
+
+func (app *application) background(fn func()) {
+	// Launch a background goroutine.
+	go func() {
+		// Recover any panic.
+		defer func() {
+			if err := recover(); err != nil {
+				app.logger.Error(fmt.Sprintf("%v", err))
+			}
+		}()
+
+		// Execute the arbitrary function that we passed as the parameter.
+		fn()
+	}()
+}
