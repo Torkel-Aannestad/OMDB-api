069949ebd8eb640c613bcd78f797aca7e4e859f5
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-11-18 11:57:33 +0100

    createUserHandler draft

---
+++ cmd/api/users.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) createUserHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	fmt.Fprintf(w, "input: %v", input)
+}
