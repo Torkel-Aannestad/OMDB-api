e22eb4ecdb1bc7a3bb5b9948c3cbd4538540caea
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-11-19 10:52:19 +0100

    user authentication done

--- cmd/api/handler_auth.go
+++ cmd/api/handler_auth.go
@@ -1,11 +1,63 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"net/http"
+	"time"
+
+	"github.com/Torkel-Aannestad/MovieMaze/internal/auth"
+	"github.com/Torkel-Aannestad/MovieMaze/internal/database"
+	"github.com/Torkel-Aannestad/MovieMaze/internal/validator"
 )
 
 func (app *application) authenticateUserHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
 
-	fmt.Fprint(w, "Hi here")
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+	database.ValidateEmail(v, input.Email)
+	database.ValidatePlaintextPassword(v, input.Password)
+	valid := v.Valid()
+	if !valid {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	user, err := app.models.Users.GetByEmail(input.Email)
+	if err != nil {
+		if errors.Is(err, database.ErrRecordNotFound) {
+			app.invalidCredentialsResponse(w, r)
+		} else {
+			app.serverErrorResponse(w, r, err)
+		}
+	}
+
+	match, err := auth.PasswordMatches(input.Password, user.PasswordHash)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+	if !match {
+		app.invalidCredentialsResponse(w, r)
+		return
+	}
+
+	authToken, err := app.models.Tokens.New(user.ID, time.Hour*24, database.ScopeAuthentication)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"authentication_token": authToken}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,47 +1,11 @@
 package auth
 
 import (
-	"crypto/rand"
-	"crypto/sha256"
-	"encoding/base32"
 	"errors"
-	"time"
 
 	"github.com/Torkel-Aannestad/MovieMaze/internal/validator"
 	"golang.org/x/crypto/bcrypt"
 )
-
-const (
-	ScopeActivation = "activation"
-)
-
-type Token struct {
-	Plaintext string
-	Hash      []byte
-	UserID    int64
-	Expiry    time.Time
-	Scope     string
-}
-
-func GenerateToken(userId int64, ttl time.Duration, scope string) (*Token, error) {
-	token := &Token{
-		UserID: userId,
-		Expiry: time.Now().Add(ttl),
-		Scope:  scope,
-	}
-
-	randomBytes := make([]byte, 16)
-	_, err := rand.Read(randomBytes)
-	if err != nil {
-		return nil, err
-	}
-
-	token.Plaintext = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomBytes)
-	hash := sha256.Sum256([]byte(token.Plaintext))
-	token.Hash = hash[:]
-
-	return token, nil
-}
 
 func ValidateTokenPlaintext(v *validator.Validator, tokenPlaintext string) {
 	v.Check(tokenPlaintext != "", "token", "must be provided")
