ea3bee1d13966e68273336ab25244ff9c5ee1b45
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-11-28 14:02:32 +0100

    categories model delete

--- internal/database/categories.go
+++ internal/database/categories.go
@@ -92,116 +92,13 @@ func (m CategoriesModel) GetCategory(id int64) (*Category, error) {
 	return &category, nil
 }
 
-// -- SELECT count(*) OVER(), id, name, parent_id, date, series_id, kind, runtime, budget, revenue, homepage, vote_average, votes_count, abstract, created_at, modified_at, version
-// -- FROM movies
-// -- WHERE (to_tsvector('simple', name) @@ plainto_tsquery('simple', sqlc.arg(name)) OR sqlc.arg(name) = '')
-// -- AND (genres @> sqlc.arg(genres) OR sqlc.arg(genres) = '{}')
-// -- ORDER BY title ASC, id ASC
-// -- LIMIT sqlc.arg(limit_value) OFFSET sqlc.arg(offset_value);
-
-// func (m CategoriesModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, Metadata, error) {
-
-// 	sortColumn := filters.getSortColumn()
-// 	sortDirection := filters.getSortDirection()
-
-// 	query := fmt.Sprintf(`
-// 		SELECT count(*) OVER(), id, created_at, title, year, runtime, genres, version
-// 		FROM movies
-// 		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
-// 		AND (genres @> $2 OR $2 = '{}')
-// 		ORDER BY %s %s, id ASC
-// 		LIMIT $3 OFFSET $4
-// 	`, sortColumn, sortDirection)
-
-// 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-// 	defer cancel()
-
-// 	args := []any{title, pg.Array(genres), filters.limit(), filters.Offset()}
-
-// 	rows, err := m.DB.QueryContext(ctx, query, args...)
-// 	if err != nil {
-// 		return nil, Metadata{}, err
-// 	}
-// 	defer rows.Close()
-
-// 	totalRecords := 0
-// 	movies := []*Movie{}
-
-// 	for rows.Next() {
-// 		var movie Movie
-
-// 		err := rows.Scan(
-// 			&totalRecords,
-// 			&movie.ID,
-// 			&movie.CreatedAt,
-// 			&movie.Title,
-// 			&movie.Year,
-// 			&movie.Runtime,
-// 			pg.Array(&movie.Genres),
-// 			&movie.Version,
-// 		)
-// 		if err != nil {
-// 			return nil, Metadata{}, err
-// 		}
-// 		movies = append(movies, &movie)
-// 	}
-
-// 	err = rows.Err()
-// 	if err != nil {
-// 		return nil, Metadata{}, err
-// 	}
-
-// 	metadata := calculateMetadata(filters.Page, filters.PageSize, totalRecords)
-
-// 	return movies, metadata, nil
-// }
-
-func (m CategoriesModel) Update(category *Category) error {
-	query := `
-	UPDATE people
-	SET 
-		name = $3,
-		birthday = $4,
-		deathday = $5,
-		gender = $6,
-		aliases = $7,
-		modified_at = $8,
-		version = version + 1
-	WHERE id = $1 and version = $2
-	RETURNING version`
-
-	args := []any{
-		&category.ID,
-		&category.Version,
-		&category.Name,
-		&category.Birthday,
-		&category.Deathday,
-		&category.Gender,
-		&category.Aliases,
-		&category.ModifiedAt,
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&category.Version)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return ErrEditConflict
-		} else {
-			return err
-		}
-	}
-	return nil
-}
-
 func (m CategoriesModel) Delete(id int64) error {
 	if id < 0 {
 		return ErrRecordNotFound
 	}
 
 	stmt := `
-		DELETE FROM people WHERE id = $1
+		DELETE FROM categories WHERE id = $1
 	`
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
