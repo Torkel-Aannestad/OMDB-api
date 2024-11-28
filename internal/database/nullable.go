a5831e4653b4a2819dabaecc49ee58b2314e75b4
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-11-28 08:32:23 +0100

    NullInt64 type for handing NULL id columns

---
+++ internal/database/nullable.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"database/sql"
+	"encoding/json"
+)
+
+type NullInt64 struct {
+	sql.NullInt64
+}
+
+func (ni *NullInt64) MarshalJSON() ([]byte, error) {
+	if !ni.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(ni.Int64)
+}
+
+func (ni *NullInt64) UnmarshalJSON(b []byte) error {
+	err := json.Unmarshal(b, &ni.Int64)
+	ni.Valid = (err == nil)
+	return err
+}
