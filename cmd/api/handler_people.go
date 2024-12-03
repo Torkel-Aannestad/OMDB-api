f6b337ffecfb75815b0dbf288d979578a1fbc32c
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-12-03 09:06:02 +0100

    get & list people handler methods

--- cmd/api/handler_people.go
+++ cmd/api/handler_people.go
@@ -11,20 +11,13 @@ import (
 	"github.com/Torkel-Aannestad/MovieMaze/internal/validator"
 )
 
-func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) createPeopleHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Name        string             `json:"name"`
-		ParentID    database.NullInt64 `json:"parent_id,omitempty"`
-		Date        time.Time          `json:"date"`
-		SeriesID    database.NullInt64 `json:"series_id,omitempty"`
-		Kind        string             `json:"kind"`
-		Runtime     int64              `json:"runtime"`
-		Budget      *float64           `json:"budget,omitempty"`
-		Revenue     *float64           `json:"revenue,omitempty"`
-		Homepage    *string            `json:"homepage,omitempty"`
-		VoteAverage float64            `json:"vote_average"`
-		VotesCount  int64              `json:"votes_count"`
-		Abstract    *string            `json:"abstract,omitempty"`
+		Name     string     `json:"name"`
+		Birthday time.Time  `json:"birthday,omitempty"`
+		Deathday *time.Time `json:"deathday,omitempty"`
+		Gender   string     `json:"gender,omitempty"`
+		Aliases  *[]string  `json:"aliases,omitempty"`
 	}
 
 	err := app.readJSON(w, r, &input)
@@ -33,65 +26,50 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	movie := database.Movie{
-		Name:        input.Name,
-		ParentID:    input.ParentID,
-		SeriesID:    input.SeriesID,
-		Date:        input.Date,
-		Kind:        input.Kind,
-		Runtime:     input.Runtime,
-		Budget:      0,
-		Revenue:     0,
-		Homepage:    "",
-		VoteAvarage: input.VoteAverage,
-		VoteCount:   input.VotesCount,
-		Abstract:    "",
+	person := database.Person{
+		Name:     input.Name,
+		Birthday: input.Birthday,
+		Gender:   input.Gender,
 	}
 
-	if input.Budget != nil {
-		movie.Budget = *input.Budget
+	if input.Deathday != nil {
+		person.Deathday = *input.Deathday
 	}
-	if input.Revenue != nil {
-		movie.Revenue = *input.Revenue
-	}
-	if input.Homepage != nil {
-		movie.Homepage = *input.Homepage
-	}
-	if input.Abstract != nil {
-		movie.Abstract = *input.Abstract
+	if input.Aliases != nil {
+		person.Aliases = *input.Aliases
 	}
 
 	v := validator.New()
-	database.ValidateMovie(v, &movie)
+	database.ValidatePeople(v, &person)
 	if !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
-	err = app.models.Movies.Insert(&movie)
+	err = app.models.People.Insert(&person)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
 	}
 
 	header := make(http.Header)
-	header.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
+	header.Set("Location", fmt.Sprintf("/v1/people/%d", person.ID))
 
-	err = app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, header)
+	err = app.writeJSON(w, http.StatusCreated, envelope{"people": person}, header)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
 	}
 
 }
-func (app *application) getMovieHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) getpeopleHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
 		app.notFoundResponse(w, r)
 		return
 	}
 
-	movie, err := app.models.Movies.Get(id)
+	person, err := app.models.People.Get(id)
 	if err != nil {
 		if errors.Is(err, database.ErrRecordNotFound) {
 			app.notFoundResponse(w, r)
@@ -102,14 +80,14 @@ func (app *application) getMovieHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	err = app.writeJSON(w, http.StatusOK, envelope{"person": person}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
 	}
 }
 
-func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) listPeopleHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
 		Name string
 		database.Filters
@@ -123,7 +101,7 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	input.Filters.Page = app.readInt(qs, "page", 1, v)
 	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
 	input.Filters.Sort = app.readString(qs, "sort", "id")
-	input.Filters.SortSafelist = []string{"id", "name", "date", "runtime", "-id", "-name", "-date", "-runtime"}
+	input.Filters.SortSafelist = []string{"id", "name", "birthday", "-id", "-name", "-birthday"}
 
 	database.ValidateFilters(v, input.Filters)
 	if !v.Valid() {
@@ -131,19 +109,19 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	movies, metadata, err := app.models.Movies.GetAll(input.Name, input.Filters)
+	people, metadata, err := app.models.People.GetAll(input.Name, input.Filters)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
 	}
 
-	err = app.writeJSON(w, http.StatusOK, envelope{"movies": movies, "metadata": metadata}, nil)
+	err = app.writeJSON(w, http.StatusOK, envelope{"people": people, "metadata": metadata}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
 }
 
-func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) updatePeopleHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
 		app.notFoundResponse(w, r)
@@ -235,7 +213,7 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 
 }
 
-func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) deletePeopleHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
 		app.notFoundResponse(w, r)
