fefa591f4b56d32642a9fcd91b8b0a682420fcee
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-12-13 13:16:00 +0100

    protectedRoutes middlware without permission

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -118,7 +118,24 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 
 }
 
-func (app *application) protectedRoute(permissionCode string, next http.HandlerFunc) http.HandlerFunc {
+func (app *application) protectedRoute(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+
+		if user.IsAnonymous() {
+			app.authenticationRequiredResponse(w, r)
+			return
+		}
+
+		if !user.Activated {
+			app.inactiveAccountResponse(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+func (app *application) protectedRoutePermission(permissionCode string, next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user := app.contextGetUser(r)
 
