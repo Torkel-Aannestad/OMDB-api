3fd0d0e2295d492a8a9b8faec8db009520ec5570
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-12-18 10:57:16 +0100

    added tokenExiredReponse for auth requests

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -88,3 +88,8 @@ func (app *application) notPermittedResponse(w http.ResponseWriter, r *http.Requ
 	message := "your user account doesn't have the necessary permissions to access this resource"
 	app.errorResponse(w, r, http.StatusForbidden, message)
 }
+
+func (app *application) tokenExiredResponse(w http.ResponseWriter, r *http.Request) {
+	message := "this request requires a recent login, please reauthenticate"
+	app.errorResponse(w, r, http.StatusForbidden, message)
+}
