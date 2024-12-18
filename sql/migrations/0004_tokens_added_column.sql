0abf5d13b72b7d4eaf48467fd82b9551fcd1976c
Torkel Aannestad <torkel.aannestad@outlook.com>
2024-12-18 10:24:25 +0100

    added data jsonb column to tokens table

--- sql/migrations/0004_tokens_added_column.sql
+++ sql/migrations/0004_tokens_added_column.sql
@@ -1,6 +1,8 @@
 -- +goose Up
 ALTER TABLE tokens ADD COLUMN created_at TIMESTAMPTZ NOT NULL DEFAULT NOW();
+ALTER TABLE tokens ADD COLUMN data JSONB not null default '{}'::jsonb;
 
 -- +goose Down
 ALTER TABLE IF EXISTS tokens DROP created_at;
+ALTER TABLE IF EXISTS tokens DROP data;
 
